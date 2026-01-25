eacc2887f599781fe2e14113b38dc9c2aeda51d0
chenzhicong <chenzhicongc@163.com>
2026-01-25 20:10:44 +0800

    test(e2e): add helper utilities export file

---
+++ backend/src/__tests__/e2e/helpers/index.ts
@@ -0,0 +1,4 @@
+export { TEST_CONFIG, TEST_USERS } from './testSetup';
+export { TestApiClient } from './testApiClient';
+export { TestWebSocketClient } from './testWebSocketClient';
+export type { ServerMessage } from './testWebSocketClient';
